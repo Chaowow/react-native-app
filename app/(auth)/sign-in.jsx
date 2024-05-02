34181cd4f4a6b0595b898925adbbc8dcbd482e52
Frankie <frankiejosefej@gmail.com>
2024-05-02 16:09:42 -0400

    Started auth screens

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,26 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, ScrollView, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { images } from '../../constants';
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView>
+        <View className='w-full justify-center h-full px-4 my-6'>
+          <Image 
+            source={images.logo} 
+            resizeMode='contain'
+            className='w-[115px] h-[70px]' 
+          />
+          
+          <Text className='text-xl text-white text-semibold mt-6 font-psemibold'>
+            Log in to GenieReel
+          </Text>
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
